b7222c6f0af3979d3d120fcf9c85acf3f5432e56
ijustwannabeblack <x.m.as.fly33@gmail.com>
2026-09-09 22:21:38 +0200

    fix: add bot setup and error handling

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', message => {
     if (message.content.startsWith('.up')) {
         const args = message.content.split(' ').slice(1);
@@ -6,3 +13,6 @@ client.on('message', message => {
         }
     }
 });
+
+// Add your bot token here
+client.login('YOUR_BOT_TOKEN');
